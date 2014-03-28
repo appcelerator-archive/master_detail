9abf0e9ca1f30b19837c9e5af22e704b67b816b9
Veer Jain <vjain@appcelerator.com>
2014-03-28 15:37:56 +0530

    Resolving issues

--- app/controllers/tablet/ApplicationWindow.js
+++ app/controllers/tablet/ApplicationWindow.js
@@ -1,10 +1,16 @@
 var args = arguments[0] || {};
+var App = require('core');
 
 //Creating the controller for the views.
 var detailView = Alloy.createController('common/DetailView');
 var masterView = Alloy.createController('common/MasterView', {
 	updateText : detailView.updateText
 });
+
+if(OS_IOS && App.Device.version >= 7){
+	$.leftContainer.top = 20;
+	$.rightContainer.top = 20;
+}
 
 //Adding the created views to the left and right pane
 $.leftContainer.add(masterView.getView());
